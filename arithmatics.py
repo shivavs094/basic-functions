d764c5157266fbe25b5b9708734cc1aaa79ae693
Shiva <108185580+shivavs094@users.noreply.github.com>
2022-07-04 21:09:14 +0530

    module_file_for_function_creating

---
+++ arithmatics.py
@@ -0,0 +1,18 @@
+def add(a,*b):
+    c = a
+    for i in b:
+        c = c + i
+    return c
+
+def sub (a,b):
+    return a-b
+
+
+def multip (a,b):
+    return a*b
+
+
+def divis (a,b):
+    return a/b
+a=1
+b=1
